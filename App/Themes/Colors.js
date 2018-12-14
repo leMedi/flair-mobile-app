67f1fd1de56ff5a43b6dc27df83bcc646dcfadbd
Mehdi ElHAIJ <mehdi.elhaij@hotmail.com>
2018-12-14 01:22:09 +0100

    Colors

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -1,5 +1,8 @@
 const colors = {
-  primary: '#00AEEF',
+  background: "#F4F2F0",
+  primary: '#5282F0',
+  secondary: '#566684',
+  darkBlue: '#062c80',
   error: "#FF001F",
   text: "#32335F",
   placeholder: "#32335F"
