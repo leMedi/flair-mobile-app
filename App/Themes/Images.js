1f708f77e38d05629baf1a51d202afe4ac9915ef
Mehdi ElHAIJ <mehdi.elhaij@hotmail.com>
2018-12-16 11:55:35 +0100

    new Assets

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -2,7 +2,11 @@
 const images = {
   Dashboard: {
     menuIcon: require('../Images/menu-icon.png'),
+    seancesIcon: require('../Images/seances-icon.png'),
+    modulesIcon: require('../Images/modules-icon.png'),
   },
+
+  moduleIcon: require('../Images/module-icon.png'),
 }
 
 export default images
